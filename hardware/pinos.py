4f69ea498efd6d1397579e1779da78d1b0033213
Sócrates Marques <118948707+socratesmarques@users.noreply.github.com>
2026-09-21 15:05:31 -0300

    refactor: mantém compatibilidade com configuração antiga de pinos

--- hardware/pinos.py
+++ hardware/pinos.py
@@ -1,5 +1,10 @@
-GPIO_ATIVO = True
-DEBOUNCE_MS = 180
+"""Compatibilidade com a configuração antiga dos pinos.
+
+Novas configurações do joystick devem ser feitas em config/joystick.py.
+"""
+
+from config.joystick import DEBOUNCE_MS, GPIO_ATIVO, JOYSTICK_PIN_KEYS
+
 PIN_SHIELD_A = 11
 PIN_SHIELD_B = 13
 PIN_SHIELD_C = 15
